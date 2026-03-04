47dc8b2b83fc4074aef6e3b29a5a125c23e077ab
Abdullah Tahir <abdullahtahir001100@gmail.com>
2026-03-04 14:08:49 +0500

    add

--- api/dbconfig/db_config.php
+++ api/dbconfig/db_config.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json");
-
+$id = $_GET['id'] ?? 0;
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
@@ -29,6 +29,16 @@ if (!mysqli_real_connect($conn, $servername, $username, $password, $database, $p
         "details" => mysqli_connect_error()
     ]);
     exit();
+}
+else {
+    if ($id) {
+         echo json_encode([
+        "success" => true,
+        "message" => "Connected to TiDB successfully!",
+        "tables" => mysqli_query($conn, "SHOW TABLES")->fetch_all(MYSQLI_ASSOC)
+    ]);
+    }
+  
 }
 
 // Connection successful!
